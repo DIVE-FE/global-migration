aec1e9895f6285451608622538810179f21742bc
이상아 <comt.mix@gmail.com>
2024-07-23 00:47:49 +0900

    Label 및 Input 원자 컴포넌트 생성 #3 (#5) (#9)
    * Feat: Label 원자 컴포넌트 생성
    
    * Feat: Input 원자 컴포넌트 생성
    
    * Feat: Label 원자 컴포넌트 추가 생성(필수문자)
    
    * Fix: 컴포넌트명 일치되도록 수정
    
    * Fix: EOL 수정
    
    ---------
    
    Co-authored-by: salee <sanga0108@smartscore.kr>

---
+++ components/atoms/Label/LabelRequired.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface LabelRequiredProps {
+  required: boolean;
+}
+
+const LabelRequired = ({ required }: LabelRequiredProps) => (
+  <>{required && <span className="required">*</span>}</>
+);
+
+export default LabelRequired;

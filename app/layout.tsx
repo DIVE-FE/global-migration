dd8446ebfe806644c3fb22f4cca8a65f188bacb5
문창규 <ckmoon@smartscore.kr>
2024-07-22 20:10:38 +0900

    레이아웃 더미 제거

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Button } from "@/components/atoms/Button/button";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Button text={"hi"} />
       <body className={inter.className}>{children}</body>
     </html>
   );

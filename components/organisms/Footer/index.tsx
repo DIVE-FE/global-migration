6e37caa6284f2e9c2f8e3ca4e776ba27a2453faa
kangminwook <108858787+wookminkang@users.noreply.github.com>
2024-07-23 19:21:51 +0900

    푸터 컴포넌트 생성 (#15)
    Co-authored-by: mwkang1 <mwkang1@smartscore.kr>

---
+++ components/organisms/Footer/index.tsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Link from "next/link";
+
+interface FamilySite {
+    name: string;
+    url: string;
+}
+
+interface FooterDataType {
+    familySites: FamilySite[];
+    address: string;
+    ceo: string;
+}
+
+const FooterData: FooterDataType = {
+    ceo: 'Inwook Yeom',
+    address: 'Business Registration No. (UEN) : 202124480N<br>Email : contact@smartscore.global 111 Somerset Road, #06-07S, 111 Somerset, Singapore<br><br>  Copyright 2023. SMARTSCORE Inc. all rights reserved.',
+    familySites: [
+        { name: 'Majesty', url: '' },
+        { name: 'Zestaim', url: '' },
+        { name: 'Mckayson', url: '' },
+        { name: 'GMK', url: '' },
+        { name: 'King\'s Rock', url: '' },
+    ],
+
+
+};
+
+
+
+const Footer: React.FC = () => {
+   return (
+       <footer className="footer">
+           <div className="footer-wrap">
+               <div className="footer-logo-box">
+                   <Link href="" className="footer-logo">
+                        <img src="https://image.smartscore.kr/smartscore_gloabl/logo/smartscore_global_logo_black.svg"  alt="footer - service logo"/>
+                   </Link>
+               </div>
+               <div className="footer-aside-box">
+                   <address>
+                       CEO : {FooterData.ceo} <br/>
+                       <div dangerouslySetInnerHTML={{ __html: FooterData.address }}></div>
+                   </address>
+
+
+                   <ul className="footer-family">
+                       {
+                           FooterData.familySites.map((site, index) => {
+                               return (
+                                   <li>
+                                       <Link href={site.url}>{site.name}</Link>
+                                   </li>
+                               )
+                           })}
+                   </ul>
+               </div>
+           </div>
+       </footer>
+   )
+}
+
+export default Footer

e03d7000ad6edbb456ec60d5909053e2ae871d6c
문창규 <ckmoon@smartscore.kr>
2024-07-24 13:25:18 +0900

    언어 선택 드롭다운 버튼 추가

---
+++ components/atoms/Dropdown/directionArrow.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image"
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+interface DerectionProp {
+    derection?: Direction;
+    className?: string;
+    style?: React.CSSProperties;
+}
+
+const DirectionArrow: React.FC<DerectionProp> = ({
+    derection = Direction.Down,
+    className,
+    style,
+}) => {
+    const arrowIamgeUrl = getArrowImageUrl(derection)
+    return (
+        <img
+            className={className}
+            style={style}
+            src={arrowIamgeUrl}
+            alt={"dropdown arrow image"}
+        />
+    )
+}
+
+function getArrowImageUrl(derection: Direction) {
+    switch (derection) {
+        case Direction.Up: return "https://image.smartscore.kr/club/icn/ic_arrow_line_up.svg"
+        case Direction.Down: return "https://image.smartscore.kr/club/icn/ic_arrow_line_down.svg"
+        case Direction.Left: return "https://image.smartscore.kr/club/icn/ic_arrow_line_left.svg"
+        case Direction.Right: return "https://image.smartscore.kr/club/icn/ic_arrow_line_right.svg"
+    }
+}
+export { Direction }
+export default DirectionArrow
---
+++ components/molecules/DropDown/index.tsx
@@ -0,0 +1,55 @@
+'use client';
+
+import CountryImageSlot from "@/components/atoms/Dropdown/coutryImageSlot";
+import DirectionArrow, { Direction } from "@/components/atoms/Dropdown/directionArrow";
+import { useEffect, useState } from "react";
+
+interface LanguageDropDownButtonProp {
+    countryCode: "EN" | "VN" | "TH" | "ID" | "CN" | "TW";
+    className?: string;
+    style?: React.CSSProperties;
+}
+
+const LanguageDropDownButton: React.FC<LanguageDropDownButtonProp> = ({
+    countryCode = "EN",
+    className,
+    style,
+}) => {
+    const [isFocus, setFocus] = useState(false);
+    const [arrowDirection, setArrowDirention] = useState(Direction.Down)
+    const focusChange = () => {
+        if (isFocus) {
+            setFocus(false)
+        } else {
+            setFocus(true)
+        }
+    }
+    useEffect(() => {
+        if (isFocus) {
+            console.log("true")
+            setArrowDirention(Direction.Up)
+        } else {
+            console.log("false")
+            setArrowDirention(Direction.Down)
+        }
+    }, [isFocus])
+    return (
+        <button
+            className={className}
+            style={style}
+            onClick={focusChange}
+            onBlur={() => {
+                console.log("onBlur")
+                setFocus(false)
+            }}
+        >
+            <CountryImageSlot className={"wh-18"} countryCode={countryCode} />
+            <div className={"mr-10"} />
+            <span>{countryCode}</span>
+            <div className={"ml-10"} />
+            <DirectionArrow className={"wh-18"} derection={arrowDirection} />
+        </button>
+    )
+}
+
+export { LanguageDropDownButton }

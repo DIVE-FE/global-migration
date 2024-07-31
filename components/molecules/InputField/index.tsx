658658a28170ed105801f033e43a33b437eb5ebb
ykh4933 <ykh4933@smartscore.kr>
2024-07-31 11:28:50 +0900

    inputField에 row props 및 labelPosition 추가

--- components/molecules/InputField/index.tsx
+++ components/molecules/InputField/index.tsx
@@ -1,10 +1,11 @@
-"use client"
+'use client';
 
-import { useState } from "react";
-import InputForm from "@/components/atoms/InputForm";
-import Label from "@/components/molecules/InputField/Label";
-import Input from "@/components/atoms/Input";
-import DeleteIcon from "@/components/atoms/icons/DeleteIcon";
+import { useState } from 'react';
+import classNames from 'classnames';
+import InputForm from '@/components/atoms/InputForm';
+import Label from '@/components/molecules/InputField/Label';
+import Input from '@/components/atoms/Input';
+import DeleteIcon from '@/components/atoms/icons/DeleteIcon';
 
 interface InputFieldProps {
   label: string;
@@ -13,7 +14,10 @@ interface InputFieldProps {
   disabled?: boolean;
   error?: boolean;
   limit?: number;
+  row?: boolean;
+  labelPosition?: 'left' | 'right' | 'top' | 'bottom';
   onChange?: (value: string) => void;
+  className?: string;
 }
 
 const InputField = ({
@@ -23,24 +27,37 @@ const InputField = ({
   error = false,
   disabled = false,
   limit = 20,
+  row = false,
+  labelPosition = 'top',
+  className,
   onChange,
 }: InputFieldProps) => {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState('');
 
-  const handleClickDeleteIcon = () => {};
+  const handleClickDeleteIcon = () => setValue('');
 
   return (
-    <InputForm className="input-type-text" error={error}>
+    <InputForm
+      className={classNames('input-type-text', className)}
+      error={error}
+      labelPosition={labelPosition}
+      row={row}
+    >
       <Label htmlFor={id} text={label} required={required} />
-      <div className="input-group">
+      <div className='input-group'>
         <Input
           id={id}
           disabled={disabled}
           value={value}
-          onChange={(e) => setValue(e.target.value)}
+          onChange={e => setValue(e.target.value)}
           maxLength={limit}
         />
-        {value && <DeleteIcon className="input-group__append" onClick={handleClickDeleteIcon} />}
+        {value && (
+          <DeleteIcon
+            className='input-group__append'
+            onClick={handleClickDeleteIcon}
+          />
+        )}
       </div>
     </InputForm>
   );

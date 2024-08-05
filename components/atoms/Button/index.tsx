3585229b16fd3c97f20515eee5f23679f32a3b18
yeomgahui <39826558+yeomgahui@users.noreply.github.com>
2024-08-05 22:25:00 +0900

    다국어 설정 (#31)
    * 다국어 관련 라이브러리 설치
    
    * 다국어 환경설정 및 세팅
    
    * 다국어 관련 폴더 이동
    
    * 메인 페이지 contactUs form 추가
    
    * Button 원복
    
    * i18n ko => en으로 수정
    
    ---------
    
    Co-authored-by: ykh4933 <ykh4933@smartscore.kr>

--- components/atoms/Button/index.tsx
+++ components/atoms/Button/index.tsx
@@ -1,26 +1,27 @@
 import { ButtonProps } from './button';
 import '@/styles/scss/import.scss';
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 import classNames from 'classnames';
 
 const Button = ({
-    children,
-    disabled,
-    type = "button",
-    className,
-    style,
-    ...eventHandlers
-}: ButtonProps) => {
-    return (
-        <button
-            disabled={disabled}
-            className={className}
-            style={style}
-            {...eventHandlers}
-        >
-            {children}
-        </button>
-    )
-}
-
-export { Button }
+  children,
+  disabled,
+  type = 'button',
+  className,
+  style,
+  variant = 'primary',
+  size,
+  ...eventHandlers
+}: ButtonProps & ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return (
+    <button
+      disabled={disabled}
+      className={classNames('button-common', variant, size, className)}
+      style={style}
+      {...eventHandlers}
+    >
+      {children}
+    </button>
+  );
+};
+export { Button };
